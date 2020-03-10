687587f2efb49133437042da3e69dbbf38217f5a
elliot alderson <r00t4f50c13ty@gmail.com>
2020-03-10 20:25:34 +0530

    custom command

--- nrjcom.sh
+++ nrjcom.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 function nrjcom()
 {
-	notify-send 'Hello world'
+	FILE=/home/ajin/Downloads/NewFile/nrjbash/sample.txt
+	
+	# get line count for $FILE (simulate 'wc -l')
+	lc=0
+	while read -r line; do
+	 ((lc++))
+	done < $FILE
+	
+	# get a random number between 1 and $lc
+	rnd=$RANDOM
+	let "rnd %= $lc"
+	((rnd++))
+	
+	# traverse file and find line number $rnd
+	i=0
+	while read -r line; do
+	 ((i++))
+	 [ $i -eq $rnd ] && break
+	done < $FILE
+	
+	# output random line
+	#echo "$line"
+	notify-send "$line"	
 }
-
+#nrjcom
